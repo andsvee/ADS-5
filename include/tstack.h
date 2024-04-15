46bb59f5a35b40dc5cc056070f04334127d34c24
andsvee <160344384+andsvee@users.noreply.github.com>
2024-04-15 21:55:53 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,27 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+ private:
+    T* stackArray;
+    int top;
+ public:
+    TStack() : top(-1) {
+      stackArray = new T[size];
+    }
+    void pop() {
+      if (top >= 0)
+        top--;
+    }
+    void push(T item) {
+      if (top < size - 1)
+        stackArray[++top] = item;
+    }
+    T get() const {
+      return stackArray[top];
+    }
+    bool checkEmpty() const {
+      return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
